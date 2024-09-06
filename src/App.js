3eb02ca6743f69c0d4456f1441ac082735fea11e
Siri <Siri>
2024-09-06 18:34:20 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { useState } from 'react';
+
 import Header from './Header/Header'; // Adjust the path based on your folder structure
 import CitySelectionModal from './Header/CitySelectionModal';// Import the modal component
 import Banner from './Banner/Banner';
+import InHouseLabs from './Banner/InHouseLabs';
+import Checkups from './Checkups/Checkups';
 
 
 function App() {
   const [isModalOpen, setModalOpen] = useState(false); // State for opening/closing modal
 
   return (
-    <div>
+    <div className="app-wrapper">
+            <div className="content-wrapper">
+
       <Header openCityModal={() => setModalOpen(true)} /> {/* Pass function to open modal */}
       {isModalOpen && <CitySelectionModal closeModal={() => setModalOpen(false)} />} {/* Modal */}
       {/* Other components like HeroBanner, TestSearch, etc. */}
       <Banner/>
+      <InHouseLabs />
+      <Checkups />
+
+</div>
     </div>
   );
 }
--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Banner.css';
 
 function Banner() {
+  const [textIndex, setTextIndex] = useState(0);
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const texts = [
+    "Your health is our priority. Book your tests and checkups now!",
+    "Experience fast and accurate reports for all your health tests.",
+    "Convenient at-home tests, available at your fingertips.",
+    "Trust our experts for comprehensive checkups and healthcare solutions."
+  ];
+
+  const images = [
+    "/images/banner.png",
+    "/images/banner2.png",
+   
+  ];
+
+  // Change the text and image every 3 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
+      setImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 6000); // Change the text and image every 3 seconds
+
+    return () => clearInterval(interval); // Cleanup interval on component unmount
+  }, [texts.length, images.length]);
+
   return (
     <div className="centered-text">
-      Your health is our priority. Book your tests and checkups now!
-      <img src="/images/banner.png" alt="Healthcare Image" class="centered-image"/>
-
-
+      <div className="text-overlay">
+      {texts[textIndex]}
+       </div>
+  
+      <img src={images[imageIndex]} alt="Healthcare" className="centered-image" />
+      
     </div>
-
   );
 }
 
